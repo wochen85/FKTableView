69aa79dc27907dd61f5a5e7485dcccac8dde0fb6
CHAT <wochen85@gmail.com>
2019-05-15 17:42:53 +0800

    1.1.2

--- FKTableView/FKTableView/Classes/FKTableView.h
+++ FKTableView/FKTableView/Classes/FKTableView.h
@@ -8,13 +8,9 @@
 #ifndef FKTableView_h
 #define FKTableView_h
 
-#import "FKCellModel.h"
-#import "FKHeaderFooterModel.h"
-#import "FKSectionModel.h"
+#import "FKTableCollectionExtensionBase.h"
 #import "UITableView+FKExtension.h"
 #import "UITableViewCell+FKExtension.h"
 #import "UITableViewHeaderFooterView+FKExtension.h"
-#import "FKHeaderFooterCommonModel.h"
-#import "UIView+FKExtension.h"
 
 #endif /* FKTableView_h */
